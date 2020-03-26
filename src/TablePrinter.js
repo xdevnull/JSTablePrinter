985fb358088687bbdd0da619a85ac25839949ca8
xdevnull <abubaker.ayman92@gmail.com>
2020-03-26 21:20:49 +0200

    COVID19

--- src/TablePrinter.js
+++ src/TablePrinter.js
@@ -1,14 +1,19 @@
 /**
- * TablePrinterOptions
+ * RowsData Type Definition
  * 
- * @typedef {{
- *  headers: !Array.<!string>,
- *  rows: !Array.<!Array.<!string>>,
- *  onOutput: function(string):void,
- *  maskChar: (string|undefined),
- *  dividerChar: (string|undefined),
- *  dividerBetweenRows: (boolean|undefined)
- * }}
+ * @typedef {!Array<!Array<!string>>} RowsData
+ */
+var RowsData;
+
+/**
+ * TablePrinter Options Type Definition
+ * 
+ * @typedef {Object} TablePrinterOptions
+ * @property {!Array<!string>} headers
+ * @property {RowsData|function(function(RowsData):void):void|Promise<RowsData>}
+ * @property {string|undefined} maskChar
+ * @property {string|undefined} dividerChar
+ * @property {boolean|undefined} dividerBetweenRows
  */
 var TablePrinterOptions;
 
@@ -69,23 +74,23 @@ var TablePrinter = (function() {
         });
     }
 
-    return {
-
-        /**
-         * Print table
-         * 
-         * @param {!TablePrinterOptions} opts 
-         */
-        print: function(opts) {
+    /**
+     * Build Table and Print
+     * 
+     * @param {TablePrinterOptions} opts 
+     * @param {RowsData} rows_data 
+     */
+    function buildTableAndPrint(opts, rows_data) {
 
             //Defaults
             opts.maskChar = opts.maskChar || "+";
             opts.dividerChar = opts.dividerChar || "-";
             opts.headers = opts.headers.map(val => ("   " + val + "   "));
-            opts.rows = opts.rows.map(row => row.map(val => ("   " + val + "   ")));
+
+            const rows = rows_data.map(row => row.map(val => ("   " + val + "   ")));
 
             //Calculate Sizes
-            const colWidths = calculateWidthSize(opts.headers, opts.rows);
+            const colWidths = calculateWidthSize(opts.headers, rows_data);
 
             //Divier, Print Mask
             const divider = createDivier(colWidths, opts.dividerChar, opts.maskChar);
@@ -95,9 +100,9 @@ var TablePrinter = (function() {
             let table = [divider, replacePrintMask(colWidths, ...opts.headers), divider];
 
             //Append rows for table
-            opts.rows.forEach((row, index) => {
+            rows_data.forEach((row, index) => {
                 table.push(replacePrintMask(colWidths, ...row));
-                if(opts.dividerBetweenRows && index + 1 < opts.rows.length) {
+                if(opts.dividerBetweenRows && index + 1 < rows_data.length) {
                     table.push(divider);
                 }
             });
@@ -108,6 +113,26 @@ var TablePrinter = (function() {
 
             //Print
             opts.onOutput(table.join("\n"));
+    }
+
+    return {
+
+        /**
+         * Print table
+         * 
+         * @param {!TablePrinterOptions} opts 
+         */
+        print: function(opts) {
+
+            // call callback
+            if(typeof opts.rows === "function") {
+                return opts.rows(rowsData => buildTableAndPrint(opts, rowsData));
+            }
+
+            // call promise
+            Promise.resolve(opts.rows).then(rowsData => buildTableAndPrint(opts, rowsData));
+
         }
-    };
+    }
+
 })();
